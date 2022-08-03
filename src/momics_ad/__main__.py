194a767a30148b2fa7a6a1182a7daa43ac90d1fb
Tomas Gonzalez Zarzar <tomasgzarzar@gmail.com>
2022-08-03 16:34:12 -0400

    updated main routine

--- src/momics_ad/__main__.py
+++ src/momics_ad/__main__.py
@@ -1,5 +1,7 @@
 import argparse
+import pandas as pd
 from .io import read
+from .stats import multi
 
 
 def main():
@@ -8,13 +10,24 @@ def main():
     '''
     parser = argparse.ArgumentParser(
         description='Multivariate multiomics sex differences')
-    parser.add_argument('-P',
-                        type=str,
-                        default='p180',
-                        metavar='PLATFORM',
-                        help='Select the platform to analyze,\
-                              either p180 or nmr.\
-                              Default: p180.')
+    parser.add_argument('-R',
+                        type=int,
+                        default=30,
+                        metavar='REPEATS',
+                        help='Number of times the CV loop should be repeated.')
+    parser.add_argument('-C',
+                        type=int,
+                        default=50,
+                        metavar='COMPONENTS',
+                        help='Maximum number of components to evaluate.')
     args = parser.parse_args()
-    dats = read.read_files(args.P)
-    print(dats)
+    m = read.read_files()
+    X = m.iloc[:, :-1]
+    Y = m.iloc[:, -1]
+    model_table = multi.plsda_doubleCV(X, Y, n_repeats=args.R,
+                                       max_components=args.C)
+    i_b_m = model_table['table'].iloc[:, 2].idxmax()
+    best_mod = model_table['models'][i_b_m]
+    xscores = pd.DataFrame(best_mod.transform(X))
+    xscores.to_csv('Xscores.csv')
+    model_table['table'].to_csv('ModelTable.csv')
